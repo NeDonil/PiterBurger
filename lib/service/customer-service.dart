ab1e5e06706bf402d1750c81bde13c73b60eb061
NeDonil <svinoukhov03@gmail.com>
2023-12-18 18:33:07 +0300

    feat(CustomerSerivce): only fetch data

---
+++ lib/service/customer-service.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+import 'package:vorstu/model/group.dart';
+import 'package:vorstu/service/auth-service.dart';
+
+import '../model/product.dart';
+
+import 'package:http/http.dart' as http;
+
+class CustomerService{
+  late Map<String, String> headers;
+  final url = "http://192.168.0.109:8080/api/customer"; // Proxy or dns
+
+  CustomerService(){
+    headers = AuthService.headers;
+  }
+
+  Future<List<Group>> getGroups() async {
+    final response = await http
+        .get(Uri.parse(url + "/groups"), headers: headers);
+
+    if(response.statusCode == 200){
+      return (jsonDecode(response.body) as List<dynamic>)
+          .map((e) => Group.fromJson(e as Map<String, dynamic>))
+          .toList();
+    } else {
+      throw Exception("Failed to load groups");
+    }
+  }
+
+  Future<List<Product>> getProductsInGroup(int groupId) async {
+    final queryParams = {"id": groupId.toString()};
+    final response = await http
+        .get(Uri.parse(url + "/grouprod").replace(queryParameters: queryParams) , headers: headers);
+
+    if(response.statusCode == 200){
+      return (jsonDecode(response.body) as List<dynamic>)
+          .map((e) => Product.fromJson(e as Map<String, dynamic>))
+          .toList();
+    } else {
+      throw Exception("Error " + response.statusCode.toString());
+    }
+  }
+}
