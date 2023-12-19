d1aea0752b6c44f440f54a5b534035cfe9432326
NeDonil <svinoukhov03@gmail.com>
2023-12-19 23:58:33 +0300

    feat: add FutureBuilder to lists

--- lib/feature/home-screen/widgets/groups-list.dart
+++ lib/feature/home-screen/widgets/groups-list.dart
@@ -24,28 +24,37 @@ class _GroupListState extends State<GroupList> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      padding: const EdgeInsets.only(top: 32),
-      itemCount: groups.length,
-      itemBuilder: (context, index) => Align(
-          alignment: Alignment.center,
-          child: Button(
-            height: 60,
-            width: 220,
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => GroupView(
-                      groupId: groups.elementAt(index).id
-                  )),
-              );
-            },
-            title: groups.elementAt(index).name, // TODO: Name
-          )
-      ),
-      separatorBuilder: (context, index) => const SizedBox(
-        height: 10,
-      ),
+    return FutureBuilder(
+        future: customerService.getManyGroups(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            return ListView.separated(
+              padding: const EdgeInsets.only(top: 32),
+              itemCount: groups.length,
+              itemBuilder: (context, index) =>
+                  Align(
+                      alignment: Alignment.center,
+                      child: Button(
+                        height: 60,
+                        width: 220,
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) =>
+                                GroupView(
+                                    groupId: groups.elementAt(index).id
+                                )),
+                          );
+                        },
+                        title: groups.elementAt(index).name, // TODO: Name
+                      )
+                  ),
+              separatorBuilder: (context, index) => const SizedBox(height: 10),
+            );
+          }
+        }
     );
   }
 }
