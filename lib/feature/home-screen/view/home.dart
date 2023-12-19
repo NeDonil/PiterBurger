102a590d74abc49a2b081ae2a35d3ad146e420c9
NeDonil <svinoukhov03@gmail.com>
2023-12-19 23:42:24 +0300

    refactor: remove groups mock

--- lib/feature/home-screen/view/home.dart
+++ lib/feature/home-screen/view/home.dart
@@ -8,30 +8,10 @@ class HomeView extends StatelessWidget {
 
   final String title;
 
-  // TODO: Подвязать реальную дату
-  static const List<Group> groups = [
-    Group(id: 1, name: 'Бургеры'),
-    Group(id: 2, name: 'Гамбургеры'),
-    Group(id: 3, name: 'Чизбургеры'),
-    Group(id: 4, name: 'Геры'),
-    Group(id: 5, name: 'Кока-колы'),
-    Group(id: 6, name: 'Коки'),
-    Group(id: 7, name: 'Колы'),
-    Group(id: 8, name: 'Свиноухов'),
-    Group(id: 9, name: 'Бургеры'),
-    Group(id: 19, name: 'Гамбургеры'),
-    Group(id: 124124, name: 'Чизбургеры'),
-    Group(id: 4142, name: 'Геры'),
-    Group(id: 144, name: 'Кока-колы'),
-    Group(id: 1121, name: 'Коки'),
-    Group(id: 111111, name: 'Колы'),
-    Group(id: 8464, name: 'Свиноухов'),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Header(),
+      appBar: const Header(),
       body: GroupList()
     );
   }
