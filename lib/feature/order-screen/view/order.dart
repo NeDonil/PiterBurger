58a6e7a9ecb560aea759d5743c84ddfee97b4d1f
NeDonil <svinoukhov03@gmail.com>
2023-12-20 17:00:27 +0300

    chore: Order button with total order price

--- lib/feature/order-screen/view/order.dart
+++ lib/feature/order-screen/view/order.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:vorstu/feature/order-screen/widgets/order-item-list.dart';
+import 'package:vorstu/service/customer-service.dart';
 import 'package:vorstu/widgets/button.dart';
 import 'package:vorstu/widgets/header.dart';
 
@@ -15,12 +16,25 @@ class OrderView extends StatefulWidget{
 
 class _OrderViewState extends State<OrderView>{
 
+  int totalPrice = 0;
+  @override
+  void initState(){
+    CustomerService.order.items.forEach((element) {
+      totalPrice += element.count * element.product.price;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: const Header(),
         body: OrderList(),
-        floatingActionButton: Button(onPressed: (){}, title: "Заказать"),
+        floatingActionButton: Button(onPressed: (){
+          setState(() {
+            CustomerService.order.items.clear();
+            totalPrice = 0;
+          });
+        }, title: "Заказать " + (totalPrice != 0 ? totalPrice.toString() + "₽": "")),
     );
   }
 }
