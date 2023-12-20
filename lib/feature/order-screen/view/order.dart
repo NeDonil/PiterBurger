6c3458e1ac7e7b4ece2af7e789eb5c864da58039
NeDonil <svinoukhov03@gmail.com>
2023-12-20 17:29:37 +0300

    feat(OrderItem): plus/minus button

--- lib/feature/order-screen/view/order.dart
+++ lib/feature/order-screen/view/order.dart
@@ -7,22 +7,12 @@ import 'package:vorstu/service/customer-service.dart';
 import 'package:vorstu/widgets/button.dart';
 import 'package:vorstu/widgets/header.dart';
 
-import '../../home-screen/widgets/groups-list.dart';
-
 class OrderView extends StatefulWidget{
   @override
   _OrderViewState createState() => _OrderViewState();
 }
 
 class _OrderViewState extends State<OrderView>{
-
-  int totalPrice = 0;
-  @override
-  void initState(){
-    CustomerService.order.items.forEach((element) {
-      totalPrice += element.count * element.product.price;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,9 +22,8 @@ class _OrderViewState extends State<OrderView>{
         floatingActionButton: Button(onPressed: (){
           setState(() {
             CustomerService.order.items.clear();
-            totalPrice = 0;
           });
-        }, title: "Заказать " + (totalPrice != 0 ? totalPrice.toString() + "₽": "")),
+        }, title: "Заказать"),
     );
   }
 }
--- lib/feature/order-screen/widgets/order-item.dart
+++ lib/feature/order-screen/widgets/order-item.dart
@@ -1,18 +1,44 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:vorstu/model/order-item.dart';
+import 'package:vorstu/service/customer-service.dart';
 
-class OrderItemWidget extends StatelessWidget{
+class OrderItemWidget extends StatefulWidget{
+  var orderItem;
+  var orderItemIdx;
+
+  OrderItemWidget({super.key, required this.orderItem, required this.orderItemIdx});
+
+  @override
+  _OrderItemWidgetState createState() => _OrderItemWidgetState(orderItem: orderItem, orderItemIdx: orderItemIdx);
+}
+
+class _OrderItemWidgetState extends State<OrderItemWidget>{
   final OrderItem orderItem;
   final int orderItemIdx;
 
-  OrderItemWidget({required this.orderItem, required this.orderItemIdx});
+  _OrderItemWidgetState({required this.orderItem, required this.orderItemIdx});
+
+  _onPlusButtonPressed(){
+    setState(() {
+      orderItem.count++;
+    });
+    super.didUpdateWidget(widget);
+  }
+
+  _onMinusButtonPressed(){
+    setState(() {
+      orderItem.count--;
+    });
+    super.didUpdateWidget(widget);
+  }
 
   @override
   Widget build(BuildContext context){
     return Container(
         padding: EdgeInsets.symmetric(horizontal: 8),
-        child: Row(
+          child: Row(
             children: [
               Row(
                 children: [
@@ -34,7 +60,7 @@ class OrderItemWidget extends StatelessWidget{
                         ),
                         SizedBox(height: 15),
                         Text(
-                            orderItem.product.price.toString() + " x " + orderItem.count.toString() +  " руб",
+                            orderItem.count.toString() + " x " + orderItem.product.price.toString() +  " руб",
                             style: TextStyle(fontSize: 16, fontFamily: 'lora', fontWeight: FontWeight.bold)
                         ),
                       ],
@@ -42,7 +68,13 @@ class OrderItemWidget extends StatelessWidget{
                   ),
                 ],
               ),
-
+              Row(
+                children:[
+                  IconButton(onPressed: _onPlusButtonPressed, icon: Icon(Icons.add, color: Color.fromRGBO(255, 227, 202, 1.0))),
+                  Text(orderItem.count.toString(), style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  IconButton(onPressed: _onMinusButtonPressed, icon: Icon(Icons.remove, color: Color.fromRGBO(255, 227, 202, 1.0),))
+                ]
+              )
             ]
         )
     );
